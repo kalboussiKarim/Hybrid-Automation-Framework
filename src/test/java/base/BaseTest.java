0c59a5adec11278715c098991d31388ac6654e88
kalboussi karim <kalboussikarim3@gmail.com>
2025-01-07 00:45:06 +0100

    added a log4j2.xml file for logging purposes

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -1,4 +1,40 @@
 package base;
 
+import org.apache.commons.lang3.RandomStringUtils;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+
+import java.time.Duration;
+
 public class BaseTest {
+
+    public WebDriver driver;
+
+    @BeforeClass
+    public void setUp(){
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().deleteAllCookies();
+        driver.get("http://localhost/opencart-4.0.2.3/upload/index.php");
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+    }
+
+    @AfterClass
+    public void tearDown(){
+        driver.quit();
+    }
+
+    public String randomString(int num){
+        return RandomStringUtils.randomAlphabetic(num);
+    }
+
+    public String randomNumber(int num){
+        return RandomStringUtils.randomNumeric(num);
+    }
+
+    public String randomAlphaNumeric(){
+        return RandomStringUtils.randomAlphabetic(3)+RandomStringUtils.randomNumeric(3);
+    }
 }
--- src/test/java/testCases/TC001AccountRegistrationTest.java
+++ src/test/java/testCases/TC001AccountRegistrationTest.java
@@ -1,31 +1,13 @@
 package testCases;
 
-import org.apache.commons.lang3.RandomStringUtils;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import base.BaseTest;
 import org.testng.Assert;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 import pageObjects.AccountRegistrationPage;
 import pageObjects.HomePage;
-import java.time.Duration;
 
 
-public class TC001AccountRegistrationTest {
-
-    WebDriver driver;
-
-
-    @BeforeClass
-    public void setUp(){
-        driver = new ChromeDriver();
-        driver.manage().window().maximize();
-        driver.manage().deleteAllCookies();
-        driver.get("http://localhost/opencart-4.0.2.3/upload/index.php");
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
-    }
-
+public class TC001AccountRegistrationTest extends BaseTest {
 
     @Test
     public void test_account_registration(){
@@ -47,16 +29,4 @@ public class TC001AccountRegistrationTest {
         Assert.assertEquals(expectedMessage,actualMessage);
     }
 
-    @AfterClass
-    public void tearDown(){
-        driver.quit();
-    }
-
-    public String randomString(int num){
-        return RandomStringUtils.randomAlphabetic(num);
-    }
-
-    public String randomNumber(int num){
-        return RandomStringUtils.randomNumeric(num);
-    }
 }
